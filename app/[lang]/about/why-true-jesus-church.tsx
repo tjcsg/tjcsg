ed1e5e3b9fcc10ee12e510568126a334aae8688e
Joshua Chin <joshuachin191@gmail.com>
2024-09-13 17:28:19 +0800

    Added AOF pictures on about page

--- app/[lang]/about/why-true-jesus-church.tsx
+++ app/[lang]/about/why-true-jesus-church.tsx
@@ -2,6 +2,7 @@ import { Locale } from '@/i18n-config';
 import { getWebContent } from '@/lib/api';
 import Container from '@/lib/components/container';
 import LinkButton from '@/lib/components/link-button';
+import ModernContentStrip from '@/lib/components/modern-content-strip';
 import { Markdown } from '@/lib/markdown';
 import adamPic from '@/public/locations/adam.jpg';
 import Image from 'next/image';
@@ -10,47 +11,71 @@ const text = {
   en: {
     title: 'Why "True" Jesus Church?',
     learnMore: 'Learn more',
+    contents: [
+      'The God whom we worship is the true God; as His followers, we uphold His nature and identify ourselves as the true church of God.',
+      'The Lord Jesus is the head of the church. The church, which is His body, is therefore called the true church.',
+      'The true church preaches the true and complete gospel, confirmed by the presence of the Holy Spirit, signs and miracles.',
+    ],
   },
   zh: {
     title: '为何称为【真】耶稣教会？',
     learnMore: 'Learn more',
+    contents: [
+      'The God whom we worship is the true God; as His followers, we uphold His nature and identify ourselves as the true church of God.',
+      'The Lord Jesus is the head of the church. The church, which is His body, is therefore called the true church.',
+      'The true church preaches the true and complete gospel, confirmed by the presence of the Holy Spirit, signs and miracles.',
+    ],
   },
 };
 
 export default async function WhyTrueJesusChurch({
   lang,
-  background,
+  background = 'bg-white',
 }: {
   lang: Locale;
-  background: string;
+  background?: string;
 }) {
   const contentfulText = await getWebContent(lang, false);
   return (
     <Container background={`${background}`}>
-      <div className={`flex flex-col md:flex-row md:justify-end`}>
-        <div className="block md:basis-2/3">
-          <h2 className="mb-8 min-w-0 text-3xl font-medium leading-7 text-gray-900  sm:tracking-tight">
-            {text[lang].title}
-          </h2>
-          <div className="prose max-w-none">
-            <Markdown content={contentfulText.aboutWhytrue} />
-          </div>
+      {/* <div className="mx-auto max-w-4xl">
+        <ModernContentStrip
+          title={text[lang].title}
+          contents={[
+            'The God whom we worship is the true God; as His followers, we uphold His nature and identify ourselves as the true church of God.',
+            'The Lord Jesus is the head of the church. The church, which is His body, is therefore called the true church.',
+            'The true church preaches the true and complete gospel, confirmed by the presence of the Holy Spirit, signs and miracles.',
+          ]}
+          otherContents={
+            <>
+              <LinkButton
+                text={text[lang].learnMore}
+                href={'tjc.org/the-one-church'}
+                type={'inverse'}
+                className={`mb-8 px-24 py-2 md:mx-0`}
+              />
+            </>
+          }
+          paragraphClasses="text-base"
+          labelClasses="max-w-32 mr-10 mb-8 text-base font-normal uppercase xl:text-lg"
+        />
+      </div> */}
+      <div className="pt-8">
+        <h2 className="mb-8 text-lg uppercase lg:text-xl">
+          {text[lang].title}
+        </h2>
+        <div className={`w-full text-pretty text-base xs:text-lg md:text-xl`}>
+          {text[lang].contents.map((content, i) => (
+            <p key={i} className={`pb-5`}>
+              {content}
+            </p>
+          ))}
           <LinkButton
             text={text[lang].learnMore}
-            href={'tjc.org/the-one-church'}
+            href={'https://tjc.org/the-one-church'}
             type={'inverse'}
-            className={`mb-8 px-24 py-2 md:mx-0`}
+            className={`px-24 py-2 md:mx-0`}
           />
-        </div>
-
-        <div className="flex items-center justify-center">
-          <div className="block">
-            <Image
-              alt="A picture of Adam Road Church"
-              src={adamPic}
-              className=" block h-auto w-full px-4"
-            ></Image>
-          </div>
         </div>
       </div>
     </Container>

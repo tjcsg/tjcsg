ffab1057a84ae37bf44e818d5ebdd5a8fae56a4b
Joshua Chin <joshuachin191@gmail.com>
2024-09-16 21:26:19 +0800

    UI changes, and made breadcrumbs show "back" if only one item

--- lib/components/header.tsx
+++ lib/components/header.tsx
@@ -27,34 +27,50 @@ export default function Header({
             </Link>
           </nav>
           <nav aria-label="Breadcrumb" className="hidden sm:flex">
+            {breadcrumbs.length == 1 && (
+              <Link
+                href={breadcrumbs[breadcrumbs.length - 1].href}
+                className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
+              >
+                <ChevronLeftIcon
+                  aria-hidden="true"
+                  className="-ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400"
+                />
+                Back
+              </Link>
+            )}
+
             <ol role="list" className="flex items-center space-x-4">
-              <li>
-                <div className="flex">
-                  <Link
-                    href={breadcrumbs[0].href}
-                    className="text-sm font-medium text-gray-500 hover:text-gray-700"
-                  >
-                    {breadcrumbs[0].name}
-                  </Link>
-                </div>
-              </li>
-              {breadcrumbs.length > 1 &&
-                breadcrumbs.slice(1).map((crumb) => (
-                  <li key={crumb.name}>
-                    <div className="flex items-center">
-                      <ChevronRightIcon
-                        aria-hidden="true"
-                        className="h-5 w-5 flex-shrink-0 text-gray-400"
-                      />
+              {breadcrumbs.length > 1 && (
+                <>
+                  <li>
+                    <div className="flex">
                       <Link
-                        href={crumb.href}
-                        className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                        href={breadcrumbs[0].href}
+                        className="text-sm font-medium text-gray-500 hover:text-gray-700"
                       >
-                        {crumb.name}
+                        {breadcrumbs[0].name}
                       </Link>
                     </div>
                   </li>
-                ))}
+                  {breadcrumbs.slice(1).map((crumb) => (
+                    <li key={crumb.name}>
+                      <div className="flex items-center">
+                        <ChevronRightIcon
+                          aria-hidden="true"
+                          className="h-5 w-5 flex-shrink-0 text-gray-400"
+                        />
+                        <Link
+                          href={crumb.href}
+                          className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                        >
+                          {crumb.name}
+                        </Link>
+                      </div>
+                    </li>
+                  ))}{' '}
+                </>
+              )}
             </ol>
           </nav>
         </div>

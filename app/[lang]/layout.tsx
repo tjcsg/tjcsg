967b87a169c0973372b2e8010a8e3a68b89aff67
Joshua Chin <joshuachin191@gmail.com>
2024-09-17 17:25:02 +0800

    Added open-graph metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -49,6 +49,14 @@ export const metadata: Metadata = {
       color: '#007da4',
     },
   },
+  openGraph: {
+    title: 'True Jesus Church Singapore',
+    description:
+      "We're True Jesus Church, a global Bible-based church. We welcome you to join God's loving family. Our goal is simple: transform lives and make disciples of Christ through preaching God's full truth of salvation.",
+    url: 'https://tjc.sg',
+    siteName: 'True Jesus Church Singapore',
+    type: 'website',
+  },
 };
 
 const notoSans = Noto_Sans({
